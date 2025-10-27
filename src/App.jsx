a76a3d5b68508e38befc56b14da4c5243c5befcb
Sabarinadh2002 <140910234+Sabarinadh2002@users.noreply.github.com>
2025-10-27 15:00:12 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import SpaceBackground from './components/SpaceBankgound';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <SpaceBackground />
+      <div style={{ position: 'relative', zIndex: 1, padding: '2rem' }}>
+        <h1 style={{ color: 'white' }}>Your Website Title</h1>
+        <p style={{ color: 'white' }}>Your content goes here</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
